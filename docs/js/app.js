c47eac6e2b5f2177ab69b570a6d895ee9d03d621
ebktech <ecabagbag@sbcglobal.net>
2017-11-22 22:48:48 -0800

    Adding frontend files to Github Pages

--- docs/js/app.js
+++ docs/js/app.js
@@ -12,12 +12,11 @@ App = {
     // Initialize web3 and set the provider to the testRPC.
     if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
     } else {
       // set the provider you want from Web3.providers
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-      web3 = new Web3(App.web3Provider);
     }
+    web3 = new Web3(App.web3Provider);
     App.displayAccountInfo();
     return App.initContract();
   },
