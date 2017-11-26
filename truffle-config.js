e97cf5724e40fd5f6800e51a2d82a2f1a1031f54
ebktech <ecabagbag@sbcglobal.net>
2017-11-25 18:27:16 -0800

    Adding frontend files to Github Pages

--- truffle-config.js
+++ truffle-config.js
@@ -5,6 +5,13 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       gas: 4600000 // Specify gas amount else you'll encounter --> Error: Exceeds block gas limit
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4, // id for rinkeby
+      from: "0x9051ad6c02655f32d3badbe6918e02cd359dca00", // From C:\Users\elmer\Documents\Ethereum.With.Baldy\rinkeby\keystore
+      gas: 4600000 // Specify gas amount else you'll encounter --> Error: Exceeds block gas limit
     }
   }
 };
